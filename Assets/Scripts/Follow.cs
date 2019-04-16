7a5bb01a816d3949650a2782dfa2edc3ee421d48
KatrineTS <32063821+KatrineTS@users.noreply.github.com>
2019-04-16 09:53:32 +0200

    Reworked the follow script

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -8,10 +8,19 @@ public class Follow : MonoBehaviour
     void Update()
     {
         Vector3 position = this.transform.position;
-
-        position.y = objectToFollow.transform.position.y;
-        position.x = objectToFollow.transform.position.x;
-
+        if (PlayerScript.isCollidingX == false && PlayerScript.isCollidingY == false)
+        {
+            position.y = objectToFollow.transform.position.y + 6.5f;
+            position.x = objectToFollow.transform.position.x;
+        }
+        else if (PlayerScript.isCollidingX == true && PlayerScript.isCollidingY == false)
+        {
+            position.y = objectToFollow.transform.position.y + 6.5f;
+        }
+        else if (PlayerScript.isCollidingY == true && PlayerScript.isCollidingX == false)
+        {
+            position.x = objectToFollow.transform.position.x;
+        }
         this.transform.position = position;
     }
 }
